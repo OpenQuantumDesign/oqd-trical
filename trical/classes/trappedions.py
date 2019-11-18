34beff70d79561d2f50156e9ca476d9ae57a362f
yhteoh <yhteoh@uwaterloo.ca>
2019-11-18 11:16:24 -0500

    Added skeleton definition for the following TrappedIons methods: __init__, equilibrium_position, normal_modes

--- trical/classes/trappedions.py
+++ trical/classes/trappedions.py
@@ -1,9 +1,24 @@
+from .potential import CoulombPotential
 import numpy as np
 
 
 class TrappedIons(object):
-    def __init__(self, **kwargss):
+    def __init__(self, N, *ps, **kwargs):
         super(IonChain, self).__init__()
+
+        self.N = N
+        self.cp = CoulombPotential(N)
+        self.ps = ps
+
+        params = {}
+        params.update(kwargs)
+        self.__dict__.update(params)
+        pass
+
+    def equilibrium_position(self, opt):
+        pass
+
+    def normal_modes(self):
         pass
 
     pass
