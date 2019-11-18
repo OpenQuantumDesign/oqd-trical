83fcd29b2b9c4b63299d92e8e3dd21c15d110dfd
yhteoh <yhteoh@uwaterloo.ca>
2019-11-18 13:17:00 -0500

    Added arguments for __init__ method of SpinLattice and SimulatedSpinLattice

--- trical/classes/spinlattice.py
+++ trical/classes/spinlattice.py
@@ -2,15 +2,21 @@ import numpy as np
 
 
 class SpinLattice(object):
-    def __init__(self):
+    def __init__(self, J):
         super(SpinLattice, self).__init__()
+
+        self.J = J
         pass
 
     pass
 
 
 class SimulatedSpinLattice(SpinLattice):
-    def __init__(self, **kwargs):
+    def __init__(self, ic, mu, Omega, **kwargs):
+        self.ic = ic
+        self.mu = mu
+        self.Omega = Omega
+
         super(SimulatedSpinLattice, self).__init__()
         pass
 
