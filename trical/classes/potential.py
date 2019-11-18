2155b026098124b542ac11fcbc14453d12b89b77
yhteoh <yhteoh@uwaterloo.ca>
2019-11-18 13:09:09 -0500

    Edited operations of Potential class to keep parameters

--- trical/classes/potential.py
+++ trical/classes/potential.py
@@ -20,26 +20,28 @@ class Potential(object):
         pass
 
     def __add__(self, other):
+        params = self.params.update(other.params)
         phi = lambda x: self.phi(x) + other.phi(x)
         dphi = lambda var: (lambda x: self.dphi(var)(x) + other.dphi(var)(x))
         d2phi = lambda var1, var2: (
             lambda x: self.d2phi(var1, var2)(x) + other.d2phi(var1, var2)(x)
         )
-        return Potential(phi, dphi, d2phi)
+        return Potential(phi, dphi, d2phi, **params)
 
     def __sub__(self, other):
+        params = self.params.update(other.params)
         phi = lambda x: self.phi(x) - other.phi(x)
         dphi = lambda var: (lambda x: self.dphi(var)(x) - other.dphi(var)(x))
         d2phi = lambda var1, var2: (
             lambda x: self.d2phi(var1, var2)(x) - other.d2phi(var1, var2)(x)
         )
-        return Potential(phi, dphi, d2phi)
+        return Potential(phi, dphi, d2phi, **params)
 
     def __mul__(self, multiplier):
         phi = lambda x: self.phi(x) * multiplier
         dphi = lambda var: (lambda x: self.dphi(var)(x) * multiplier)
         d2phi = lambda var1, var2: (lambda x: self.d2phi(var1, var2)(x) * multiplier)
-        return Potential(phi, dphi, d2phi)
+        return Potential(phi, dphi, d2phi, **self.params)
 
     def __rmul__(self, multiplier):
         return self * multiplier
@@ -48,7 +50,7 @@ class Potential(object):
         phi = lambda x: self.phi(x) / divisor
         dphi = lambda var: (lambda x: self.dphi(var)(x) / divisor)
         d2phi = lambda var1, var2: (lambda x: self.d2phi(var1, var2)(x) / divisor)
-        return Potential(phi, dphi, d2phi)
+        return Potential(phi, dphi, d2phi, **self.params)
 
     def __call__(self, x):
         return self.phi(x)
