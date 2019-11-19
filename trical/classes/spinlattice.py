453d415170de25caa4781ead1c409f139e78af0b
yhteoh <yhteoh@uwaterloo.ca>
2019-11-19 12:30:34 -0500

    Added skeleton docstrings for spinlattice submodule of classes

--- trical/classes/spinlattice.py
+++ trical/classes/spinlattice.py
@@ -1,8 +1,22 @@
+"""SUMMARY
+"""
 import numpy as np
 
 
 class SpinLattice(object):
+
+    """SUMMARY
+    
+    Attributes:
+        J (TYPE): DESCRIPTION
+    """
+    
     def __init__(self, J):
+        """SUMMARY
+        
+        Args:
+            J (TYPE): DESCRIPTION
+        """
         super(SpinLattice, self).__init__()
 
         self.J = J
@@ -12,7 +26,24 @@ class SpinLattice(object):
 
 
 class SimulatedSpinLattice(SpinLattice):
+
+    """SUMMARY
+    
+    Attributes:
+        ic (TYPE): DESCRIPTION
+        mu (TYPE): DESCRIPTION
+        Omega (TYPE): DESCRIPTION
+    """
+    
     def __init__(self, ic, mu, Omega, **kwargs):
+        """SUMMARY
+        
+        Args:
+            ic (TYPE): DESCRIPTION
+            mu (TYPE): DESCRIPTION
+            Omega (TYPE): DESCRIPTION
+            **kwargs: DESCRIPTION
+        """
         self.ic = ic
         self.mu = mu
         self.Omega = Omega
