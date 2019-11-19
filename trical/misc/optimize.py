de8aeb7b778df235b810a782392bb75cf66cced1
yhteoh <yhteoh@uwaterloo.ca>
2019-11-19 17:25:44 -0500

    Fixed minor bug in dflt_opt in optimize submodule of misc

--- trical/misc/optimize.py
+++ trical/misc/optimize.py
@@ -16,11 +16,11 @@ def dflt_opt(ti):
         func(func(1-D array of float) -> 1-D array float): Optimizer for a function f 
         that returns the minimum value of f
     """
-    if self.dim == 1:
-        x_guess = np.linspace(-(self.N - 1) / 2, (self.N - 1) / 2, self.N)
+    if ti.dim == 1:
+        x_guess = np.linspace(-(ti.N - 1) / 2, (ti.N - 1) / 2, ti.N)
     else:
         x_guess = np.append(
-            np.concatenate([np.zeros(self.N)] * (self.dim - 1)),
-            np.linspace(-(self.N - 1) / 2, (self.N - 1) / 2, self.N),
+            np.concatenate([np.zeros(ti.N)] * (ti.dim - 1)),
+            np.linspace(-(ti.N - 1) / 2, (ti.N - 1) / 2, ti.N),
         )
     return lambda f: opt.minimize(f, x_guess, method="SLSQP").x
