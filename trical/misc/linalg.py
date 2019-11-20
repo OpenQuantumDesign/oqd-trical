47862e6712eef3ad770499f8e690f0332325e47f
yhteoh <yhteoh@uwaterloo.ca>
2019-11-20 18:30:36 -0500

    Defined coordinate transformation between Cartesian and spherical coordinates as cartesian_to_spherical and spherical_to_cartesian functions in linalg submodule of misc

--- trical/misc/linalg.py
+++ trical/misc/linalg.py
@@ -15,3 +15,17 @@ def norm(x):
         (N-1)-dim array of float: Norm of the array of interest over the last axis
     """
     return np.sqrt((x ** 2).sum(-1))
+
+
+def cartesian_to_spherical(x, y, z):
+    r = np.hypot(np.hypot(x, y), z)
+    phi = np.arctan2(np.hypot(x, y), z)
+    theta = np.arctan2(y, x)
+    return r, phi, theta
+
+
+def spherical_to_cartesian(r, phi, theta):
+    x = r * np.sin(phi) * np.cos(theta)
+    y = r * np.sin(phi) * np.sin(theta)
+    z = r * np.cos(phi)
+    return x, y, z
