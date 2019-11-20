6e12ccd65a941b4f6941623e0e7f870d1efe8e80
yhteoh <yhteoh@uwaterloo.ca>
2019-11-20 10:08:29 -0500

    Added docstring for setalg submodule of misc

--- trical/misc/setalg.py
+++ trical/misc/setalg.py
@@ -1,2 +1,17 @@
-def intersection(a1,a2):
+"""
+Relevant set algebra functions for TrICal
+"""
+
+
+def intersection(a1, a2):
+    """
+    Takes the intersection between two arrays
+
+    Args:
+        a1 (1-D array): First array
+        a2 (1-D array): Second array
+
+    Returns:
+        1-D array: Intersection between the two given arrays
+    """
     return [i for i in a1 if i in a2]
