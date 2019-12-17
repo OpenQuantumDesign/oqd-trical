60b4c8482245bce1c618dd7d80e40c19960dcff5
yhteoh <yhteoh@uwaterloo.ca>
2019-12-16 20:05:13 -0500

    Moved constants, constants now submodule of misc

---
+++ trical/misc/constants.py
@@ -0,0 +1,12 @@
+"""[summary]
+
+:Variables:
+    * **e** (float) -- [description]
+    * **hbar** (float) -- [description]
+    * **k** (float) -- [description]
+    * **m_a** (dict) -- [description]
+"""
+e = 1.602176621e-19
+hbar = 1.054571818e-34
+k = 8.987551792e09
+m_a = {"Ba133": 2.206956176e-25, "Yb171": 2.838464469e-25}
