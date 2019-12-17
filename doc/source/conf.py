3dc2757235684718833393fb60e3b4cdd97f978d
yhteoh <yhteoh@uwaterloo.ca>
2019-12-17 12:14:52 -0500

    Updated documentation

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,7 +12,10 @@
 
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.join(__file__,"../../..")))
+
+sys.path.append(os.path.abspath(os.path.join(__file__, "../../..")))
+
+import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
@@ -27,7 +30,11 @@ author = "QITI"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx_rtd_theme",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -42,7 +49,7 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "default"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
