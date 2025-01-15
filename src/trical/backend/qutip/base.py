e465257d94c1d63fa5f045a16b5cfe938b95eb6e
yhteoh <yhteoh@uwaterloo.ca>
2025-01-15 12:44:20 -0500

    Defined QutipBackend

--- src/trical/backend/qutip/base.py
+++ src/trical/backend/qutip/base.py
@@ -1,5 +1,16 @@
 from oqd_core.backend.base import BackendBase
 
+from oqd_compiler_infrastructure import In, Chain, Post, Pre
+
+########################################################################################
+
+from ...light_matter.analysis import AnalyseHilbertSpace
+from ...light_matter.codegen import ConstructHamiltonian
+from ...light_matter.canonicalize import canonicalization_pass
+
+from .codegen import QutipCodeGeneration
+from .vm import QutipVM
+
 ########################################################################################
 
 
@@ -7,8 +18,32 @@ class QutipBackend(BackendBase):
     def __init__(self):
         pass
 
-    def compile(self, circuit):
-        pass
+    def compile(self, circuit, fock_cutoff):
+        analysis = In(AnalyseHilbertSpace())
 
-    def run(self, experiment):
-        pass
+        analysis(circuit)
+
+        hilbert_space = analysis.children[0].hilbert_space
+
+        compiler = Chain(
+            Post(ConstructHamiltonian()),
+            canonicalization_pass,
+            Post(QutipCodeGeneration(fock_cutoff=fock_cutoff)),
+        )
+
+        experiment = compiler(circuit)
+
+        return experiment, hilbert_space, fock_cutoff
+
+    def run(self, experiment, hilbert_space, fock_cutoff, timestep):
+        vm = Pre(
+            QutipVM(
+                hilbert_space=hilbert_space,
+                fock_cutoff=fock_cutoff,
+                timestep=timestep,
+            )
+        )
+
+        vm(experiment)
+
+        return vm.children[0].result
